94c0cecf038cc25aec29eeae4f6f06aa24ea1a5f
Wildjacky22 <62379@bwls.net>
2016-10-27 16:31:43 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,18 @@
-
+function setup() {
+  createCanvas( windowWidth , windowHeight );
+  
+  noStroke();
+  
+  background( 0 , 0 , 0 )
+}
+function draw() {
+  var x = random( 0 , windowWidth );
+  var y = random( 0 , windowHeight);
+  var d = random( 0 , 125);
+  var r = random( 0 , 255);
+  var g = random( 0 , 255);
+  var b = random( 0 , 255);
+  var a = random( 0 , 200);
+  ellipse( x , y , d , d);
+    fill( r , g , b , 100 );
+}
